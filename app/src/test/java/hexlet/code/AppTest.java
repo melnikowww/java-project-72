b105abd72d90d94b53c81541dbe3c2e851751a6e
Владимир <melnikoffspb@ya.ru>
2023-08-23 22:59:36 +0300

    6 step

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -3,12 +3,15 @@ package hexlet.code;
 
 import hexlet.code.domain.Url;
 import hexlet.code.domain.query.QUrl;
+import io.ebean.DB;
+import io.ebean.Database;
 import io.javalin.Javalin;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,6 +20,7 @@ public class AppTest {
 
     private static Javalin app;
     private static String baseUrl;
+    private static Database database;
 
     @BeforeAll
     public static void beforeAll() {
@@ -24,6 +28,7 @@ public class AppTest {
         app.start();
         int port = app.port();
         baseUrl = "http://localhost:8080";
+        database = DB.getDefault();
     }
 
     @AfterAll
@@ -31,21 +36,36 @@ public class AppTest {
         app.stop();
     }
 
+    @BeforeEach
+    void beforeEach() {
+//        database.script().run("/truncate.sql");
+        database.script().run("/seed-test.sql");
+    }
+
     @Test
-    public void mainPageTest() {
-        HttpResponse<String> response = Unirest.get(baseUrl).asString();
+    public void testNewUrl() {
+        HttpResponse<String> response = Unirest
+            .get(baseUrl)
+            .asString();
         assertThat(response.getStatus()).isEqualTo(200);
         assertThat(response.getBody()).contains("Анализатор страниц");
     }
 
     @Test
-    public void testCreate() {
+    public void testCreateUrl() {
         String name = "https://ebean.io";
 
-        HttpResponse response = Unirest
+        HttpResponse responsePost = Unirest
             .post(baseUrl + "/urls")
             .field("url", name)
             .asEmpty();
+
+        assertThat(responsePost.getStatus()).isEqualTo(302);
+        assertThat(responsePost.getHeaders().getFirst("Location")).isEqualTo("/urls");
+
+        HttpResponse response = Unirest
+            .get(baseUrl + "/urls")
+                .asString();
 
         assertThat(response.getStatus()).isEqualTo(200);
         assertThat(response.getBody().toString()).contains(name);
@@ -57,5 +77,33 @@ public class AppTest {
 
         assertThat(actualUrl).isNotNull();
         assertThat(actualUrl.getName()).isEqualTo(name);
+    }
+
+    @Test
+    public void testListUrls() {
+        HttpResponse response = Unirest
+            .get(baseUrl + "/urls")
+            .asString();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getBody().toString()).contains("Сайты");
+    }
+
+    @Test
+    public void testShowUrl() {
+        String name = "https://ebean.io";
+
+        HttpResponse responsePost = Unirest
+            .post(baseUrl + "/urls")
+            .field("url", name)
+            .asEmpty();
+
+        HttpResponse response = Unirest
+            .get(baseUrl + "/urls/1")
+            .asString();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getBody().toString()).contains("Сайт " + name);
+
     }
 }
---
+++ app/src/test/resources/seed-test.sql
@@ -0,0 +1,2 @@
+--INSERT INTO url (id, name, createdAt) VALUES
+--  (1, 'https://github.com', '23/08/2022');

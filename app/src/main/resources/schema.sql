5bb98131d681c16c53e84cac0ba584d93e093e67
Владимир <melnikoffspb@ya.ru>
2023-09-13 20:37:57 +0300

    Migration to JDBC

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP
 );
@@ -9,7 +9,7 @@ CREATE TABLE urls (
 DROP TABLE IF EXISTS url_checks;
 
 CREATE TABLE url_checks (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     status_code INT NOT NULL,
     title VARCHAR(255) NOT NULL,
     h1 VARCHAR(255) NOT NULL,

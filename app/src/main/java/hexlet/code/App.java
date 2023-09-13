2da4df25cf61f101a7148e0bf11a2f1259515fb8
Владимир <melnikoffspb@ya.ru>
2023-09-13 23:06:41 +0300

    Migration to JDBC

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -49,20 +49,23 @@ public class App {
     }
 
     public static Javalin getApp() throws IOException, SQLException {
-        Properties props = new Properties();
-        props.setProperty("jdbcUrl", getDatabaseUrl());
-
-        if (isProd()) {
-            props.setProperty("dataSource.user", System.getenv("JDBC_DATABASE_USERNAME"));
-            props.setProperty("dataSource.password", System.getenv("JDBC_DATABASE_PASSWORD"));
-            props.setProperty("dataSource.portNumber", System.getenv("PORT"));
-        } else {
-            props.setProperty("dataSource.user", "user");
-            props.setProperty("dataSource.password", "user");
-        }
+//        Properties props = new Properties();
+//        props.setProperty("jdbcUrl", getDatabaseUrl());
+//
+//        if (isProd()) {
+//            props.setProperty("dataSource.user", System.getenv("JDBC_DATABASE_USERNAME"));
+//            props.setProperty("dataSource.password", System.getenv("JDBC_DATABASE_PASSWORD"));
+//            props.setProperty("dataSource.portNumber", System.getenv("PORT"));
+//        } else {
+//            props.setProperty("dataSource.user", "user");
+//            props.setProperty("dataSource.password", "user");
+//        }
 
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(getDatabaseUrl());
+        hikariConfig.setUsername(System.getenv("JDBC_DATABASE_USERNAME"));
+        hikariConfig.setPassword(System.getenv("JDBC_DATABASE_PASSWORD"));
+
         HikariDataSource dataSource = new HikariDataSource(hikariConfig);
 
         URL schema = App.class.getClassLoader().getResource("schema.sql");
